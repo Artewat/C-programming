62bf66caa7f29b5a5cf180033af40cabc32f614c
Artewat <zexerarer@gmail.com>
2025-02-15 14:01:18 +1000

    lab1

--- lab1.c
+++ lab1.c
@@ -3,9 +3,10 @@
 int main()
 {
     int a, b, c;
+    printf("Print a b c: ");
     scanf("%d %d %d", &a, &b, &c);
     int D = b * b - 4 * a * c;
     float x1 = (-1*b + sqrt(D))/(2*a);
     float x2 = (-1*b - sqrt(D))/(2*a);
-    printf("First x: %f\nSecond x: %f", x1, x2);
+    printf("First x: %.2f\nSecond x: %.2f", x1, x2);
 }
