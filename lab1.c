4079b6912a14f4d902cd8cee160faa9953979936
Artewat <zexerarer@gmail.com>
2025-02-17 15:49:51 +1000

    lab2

--- lab1.c
+++ lab1.c
@@ -2,11 +2,19 @@
 #include <math.h>
 int main()
 {
+	printf("A program for finding x's of a quadratic equation using a discriminant.\n");
     int a, b, c;
     printf("Print a b c: ");
     scanf("%d %d %d", &a, &b, &c);
     int D = b * b - 4 * a * c;
+    if(D >= 0)
+    {
     float x1 = (-1*b + sqrt(D))/(2*a);
     float x2 = (-1*b - sqrt(D))/(2*a);
     printf("First x: %.2f\nSecond x: %.2f", x1, x2);
+    }
+    else
+    {
+        printf("The discriminant is less than zero.\n");
+    }
 }
